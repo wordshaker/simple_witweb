1a99e47d94242ec269fa7b5ccac620d32a90a2c2
Jessica White <code.jessw@gmail.com>
2017-06-22 19:58:10 +0100

    edit deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -85,20 +85,22 @@ goto :EOF
 :: Deployment
 :: ----------
 
-::Deployment
+:Deployment
 echo Handling node.js deployment.
 
 :: 1. Select node version
 call :SelectNodeVersion
 
 :: 2. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  ::  pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
+  :: popd
 )
 
 :: 3. Execute Gulp
-IF EXIST "%DEPLOYMENT_SOURCE%\gulpfile.js" (
+IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
     call .\node_modules\.bin\gulp build
     IF !ERRORLEVEL! NEQ 0 goto error
 )
@@ -108,6 +110,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%\dist" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
+
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
 
